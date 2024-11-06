655a18d03b613510fe7cad45c12039a42f258e42
KBaybekov <91565993+KBaybekov@users.noreply.github.com>
2024-11-06 18:36:09 +0300

    nanopore_processing created

--- src/utils.py
+++ src/utils.py
@@ -1,30 +1,78 @@
 import os
 import yaml
 
-def get_folder_structure(directory: str) -> dict:
-    """
-    Рекурсивно обходит папки и файлы, создавая структуру словаря.
-    
-    :param directory: Путь к директории, которую нужно проанализировать
-    :return: Словарь с отображением структуры папок и файлов
-    """
-    structure = {}
-    for item in os.listdir(directory):
-        item_path = os.path.join(directory, item)
-        if os.path.isdir(item_path):
-            structure[item] = get_folder_structure(item_path)  # Рекурсивный вызов для подкаталогов
-        else:
-            structure[item] = None  # Файл
-        print(f'\r{len(structure)}', end="")
-    return structure
 
-def save_structure_to_yaml(directory: str, output_file: str):
+def get_fast5_files(dir:str) -> dict:
+    subdirs = [os.path.join(dir, s) for s in os.listdir(dir) if os.path.isdir(os.path.join(dir, s, ))]
+    sample_data = {}
+    for subdir in subdirs:
+        fast5s = []
+        for root, _ds, fs in os.walk(subdir):
+            for f in fs:
+                if f.endswith('.fast5') and os.path.basename(root) == 'fast5_pass':
+                    fast5s.append(os.path.join(root, f))
+        if fast5s:
+            sample_data[os.path.basename(subdir)] = fast5s
+    if not sample_data:
+        raise FileNotFoundError('FAST5 файлы не найдены!')
+    return sample_data
+
+
+
+
+
+
+
+    files = [os.path.join(dir, s) for s in os.listdir(dir) if os.path.isdir({os.path.join(dir, s, )}) and os.path]
+    if not files:
+        raise FileNotFoundError("Образцы не найдены. Проверьте входные и исключаемые образцы, а также директорию с исходными файлами.")
+    return files
+
+def get_samples_in_dir(dir:str, extensions:tuple):
     """
-    Сохраняет структуру папки в YAML-файл.
+    Генерирует список файлов на основе включающих и исключающих образцов.
+    Выдаёт ошибку, если итоговый список пустой.
+
+    :param dir: Директория, где искать файлы.
+    :param extensions: Расширения файлов для поиска.
+    :return: Список путей к файлам.
+    """
+    # Ищем все файлы в директории с указанными расширениями
+    files = [os.path.join(dir, s) for s in os.listdir(dir) if s.endswith(extensions)]
+    if not files:
+        raise FileNotFoundError("Образцы не найдены. Проверьте входные и исключаемые образцы, а также директорию с исходными файлами.")
+    return files
+
+
+def get_samples_in_dir_tree(dir:str, extensions:tuple) -> list:
+    """
+    Генерирует список файлов, проходя по дереву папок, корнем которого является dir.
+    Выдаёт ошибку, если итоговый список пустой.
+
+    :param dir: Директория, где искать файлы.
+    :param extensions: Кортеж расширений файлов для поиска.
+    :return: Список файлов с путями.
+    """
+    files = []
+    for root, _ds, fs in os.walk(dir):
+        samples = [os.path.join(root, f) for f in fs 
+                    if f.endswith(extensions)]
+        files.extend(samples)
+    if not files:
+        raise FileNotFoundError("Образцы не найдены. Проверьте входные и исключаемые образцы, а также директорию с исходными файлами.")
+    return files
+
+def save_yaml(filename, path, data):
+    """
+    Сохраняет словарь в файл в формате YAML.
     
-    :param directory: Путь к директории, которую нужно проанализировать
-    :param output_file: Путь к YAML-файлу, куда будет сохранена структура
+    :param filename: Имя файла для сохранения (например, 'config.yaml')
+    :param path: Путь к директории, где будет сохранён файл
+    :param data: Словарь с данными, которые нужно сохранить в YAML
     """
-    structure = get_folder_structure(directory)
-    with open(output_file, 'w') as yaml_file:
-        yaml.dump(structure, yaml_file, default_flow_style=False, sort_keys=False)
+    # Полный путь к файлу
+    file_path = f'{path}{filename}.yaml'
+
+    # Записываем данные в YAML-файл
+    with open(file_path, 'w') as yaml_file:
+        yaml.dump(data, yaml_file, default_flow_style=False, sort_keys=False)
